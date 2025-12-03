856144b05172329f6630676b03bff9e0be033586
christopher-dembski <christopher_dembski@outlook.com>
2025-12-03 11:11:39 -0500

    Day 3. Part 2.

--- d3.py
+++ d3.py
@@ -4,7 +4,7 @@ def parse_input(file_name):
         return tuple(tuple(int(n) for n in bank) for bank in banks)
 
 
-def max_joltage(bank):
+def max_joltage_part_one(bank):
     max_digit_seen = 0
     max_value = 0
     for n in bank:
@@ -13,9 +13,19 @@ def max_joltage(bank):
     return max_value
 
 
-def part_one(file_name):
+def max_joltage_part_two(bank):
+    maxes = [0 for _ in range(13)]  # maxes[i] = max value that can be made with i digits
+    for i, n in enumerate(bank):
+        for num_digits in range(min(i + 1, 12), 0, -1):
+            maxes[num_digits] = max(maxes[num_digits], maxes[num_digits - 1] * 10 + n)
+    return maxes[12]
+
+
+def solve(file_name, part):
     banks = parse_input(file_name)
+    max_joltage = max_joltage_part_one if part == 1 else max_joltage_part_two
     return sum(max_joltage(bank) for bank in banks)
 
 
-print(part_one('inputs/d3b.txt'))
+print(solve('inputs/d3b.txt', 1))
+print(solve('inputs/d3b.txt', 2))
