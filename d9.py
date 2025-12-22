418eff26d2e8955b4e299199b326c1cc85b5a476
christopher-dembski <christopher_dembski@outlook.com>
2025-12-22 10:20:57 -0500

    Day 9. Part 2.

--- d9.py
+++ d9.py
@@ -1,3 +1,6 @@
+import itertools as it
+
+
 def parse_input(file_name):
     with open(file_name) as file:
         return tuple(tuple(int(n) for n in line.split(','))
@@ -10,9 +13,111 @@ def area(corner_1, corner_2):
     return x_dim * y_dim
 
 
+def colour_path_and_adjacent(reds):
+    greens = set()
+    whites = set()
+    reds_offset_by_one = (*reds[1::], reds[0])
+    for (x1, y1), (x2, y2) in zip(reds, reds_offset_by_one):
+        # print((x1, y1), (x2, y2))
+        if x1 == x2:  # vertical
+            if y1 < y2:  # moving down
+                for y in range(y1, y2 + 1):
+                    current = (x1, y)
+                    if current not in reds:
+                        greens.add(current)
+                    adjacent_right = (x1 + 1, y)
+                    if adjacent_right not in greens and adjacent_right not in reds:
+                        whites.add(adjacent_right)
+            else:  # moving up
+                for y in range(y1, y2 - 1, -1):
+                    current = (x1, y)
+                    if current not in reds:
+                        greens.add(current)
+                    adjacent_left = (x1 - 1, y)
+                    if adjacent_left not in greens and adjacent_left not in reds:
+                        whites.add(adjacent_left)
+        else:  # horizontal
+            if x1 < x2:  # moving right
+                for x in range(x1, x2 + 1):
+                    current = (x, y1)
+                    if current not in reds:
+                        greens.add(current)
+                    adjacent_above = (x, y1 - 1)
+                    if adjacent_above not in greens and adjacent_above not in reds:
+                        whites.add(adjacent_above)
+            else:  # moving left
+                for x in range(x1, x2 - 1, -1):
+                    current = (x, y1)
+                    if current not in reds:
+                        greens.add(current)
+                    adjacent_below = (x, y1 + 1)
+                    if adjacent_below not in greens and adjacent_below not in reds:
+                        whites.add(adjacent_below)
+    return greens, whites
+
+
+# (x1, y1) is top-left corner
+# (x2, y2) is bottom-right corner
+def valid_rectangle(x1, y1, x2, y2, whites):
+    # is this logic right?
+    x1, x2 = sorted([x1, x2])
+    y1, y2 = sorted([y1, y2])
+    # horiztonal-vertical
+    if any((x, y1) in whites for x in range(x1 + 1, x2 + 1)):
+        return False
+    if any((x2, y) in whites for y in range(y1 + 1, y2 + 1)):
+        return False
+    # vertical-horizontal
+    if any((x1, y) in whites for y in range(y1 + 1, y2 + 1)):
+        return False
+    if any((x, y2) in whites for x in range(x1 + 1, x2 + 1)):
+        return False
+    # did not encounter white space on either path
+    return True
+
+
+def display_grid(reds, greens, whites):
+    max_x = max(x for x, y in it.chain(reds, greens, whites))
+    max_y = max(y for x, y in it.chain(reds, greens, whites))
+    for y in range(0, max_y + 1):
+        for x in range(0, max_x + 1):
+            p = (x, y)
+            symbol = 'R' if p in reds else 'G' if p in greens else 'W' if p in whites else '.'
+            print(symbol, end='')
+        print()
+    print()
+
+
 def part_one(file_name):
     positions = parse_input(file_name)
     return max(area(corner_1, corner_2) for corner_1 in positions for corner_2 in positions)
 
 
-print(part_one('inputs/d9b.txt'))
+def part_two(file_name):
+    reds = parse_input(file_name)
+    greens, whites = colour_path_and_adjacent(reds)
+    whites -= greens  # TO DO: fix this correctly
+    # display_grid(reds, greens, whites)
+    # exclude too many (not always top-right bottom-left)
+    # must consider others
+    count = 0
+    limit = 10
+    mx = 0
+    for red_1 in reds:
+        for red_2 in reds:
+            if valid_rectangle(*red_1, *red_2, whites):
+                mx = max(mx, area(red_1, red_2))
+            count += 1
+            if count > limit:
+                print(f'Linit: {limit}')
+                limit *= 10
+    return mx
+    # valid_rectangles = [(red_1, red_2) for red_1 in reds for red_2 in reds
+    #                     if valid_rectangle(*red_1, *red_2, whites)]
+    # for r1, r2 in valid_rectangles:
+    #     print(r1, r2)
+    # return max(area(red_1, red_2) for red_1, red_2 in valid_rectangles)
+
+
+print(part_two('inputs/d9a.txt'))
+print(part_two('inputs/d9b.txt'))
