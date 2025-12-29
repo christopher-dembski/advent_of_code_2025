6df9406299bcaf94c66be3e5043f2411cd7e877f
christopher-dembski <christopher_dembski@outlook.com>
2025-12-28 22:03:25 -0500

    Day 11. Part 2.

--- d11.py
+++ d11.py
@@ -10,30 +10,41 @@ def parse_input(file_name):
 
 
 def incoming_edges_from_outgoing_edges(outgoing_edges):
-    incoming_edges = {node: set() for node in (*outgoing_edges, 'out')}
+    incoming_edges = {node: set() for node in (*outgoing_edges.keys(), 'out')}
     for node, neighbours in outgoing_edges.items():
         for neighbour in neighbours:
             incoming_edges[neighbour].add(node)
     return incoming_edges
 
 
-def num_paths(incoming_edges):
-    paths_memo = {'you': 1}
+def num_paths(start, destintion, incoming_edges):
+    memo = {start: 1}
 
     def num_paths_recurse(destination):
-        if destination in paths_memo:
-            return paths_memo[destination]
+        if destination in memo:
+            return memo[destination]
         num = sum(num_paths_recurse(incoming) for incoming in incoming_edges[destination])
-        paths_memo[destination] = num
+        memo[destination] = num
         return num
 
-    return num_paths_recurse('out')
+    return num_paths_recurse(destintion)
 
 
 def part_one(file_name):
     outgoing_edges = parse_input(file_name)
     incoming_edges = incoming_edges_from_outgoing_edges(outgoing_edges)
-    return num_paths(incoming_edges)
+    return num_paths('you', 'out', incoming_edges)
+
+
+def part_two(file_name):
+    outgoing_edges = parse_input(file_name)
+    incoming_edges = incoming_edges_from_outgoing_edges(outgoing_edges)
+    svr_fft = num_paths('svr', 'fft', incoming_edges)
+    fft_dac = num_paths('fft', 'dac', incoming_edges)
+    dac_fft = num_paths('dac', 'fft', incoming_edges)
+    dac_out = num_paths('dac', 'out', incoming_edges)
+    return svr_fft * (fft_dac if fft_dac > 0 else dac_fft) * dac_out
 
 
 print(part_one('inputs/d11b.txt'))
+print(part_two('inputs/d11b.txt'))
